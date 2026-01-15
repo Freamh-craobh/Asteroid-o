f4ed76803155829c7372a0d2ab16b2371308a146
Freamh-craobh <oisin.duke@gmail.com>
2026-01-15 19:58:40 +0000

    added asteroids movement

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, player
+import pygame, player, asteroid, asteroidfield
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 import constants
@@ -9,11 +9,20 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    player.Player.containers = (updatable, drawable)
+    asteroid.Asteroid.containers = (asteroids, updatable, drawable)
+    asteroidfield.AsteroidField.containers = (updatable)
+
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player1 = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = asteroidfield.AsteroidField()
 
     while True:
         log_state()
@@ -23,9 +32,11 @@ def main():
             pass
         screen.fill("black")
 
-        player1.draw(screen)
-        player1.update(dt)
-
+        for obj in drawable:
+            obj.draw(screen)
+        for obj in updatable:
+            obj.update(dt)
+        
         pygame.display.flip()
 
         
